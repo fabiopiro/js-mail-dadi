4b4279d5f96a5cad113e50f28c8df78332508308
fabiopiro <piroddifabio01@gmail.com>
2021-04-28 15:59:33 +0200

    email funzionante

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,3 +10,30 @@
 // 4. torniamo a scrivere in italiano
 // 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
+
+
+// Chiedi all'utente la sua email
+var userEmail = prompt ("Qual è la tua email? [rosso/verde/blu/giallo/arancione@mail.com]");
+console.log(userEmail);
+
+// Lista
+var listaEmail = ["rosso@mail.com" , "verde@mail.com" , "blu@mail.com" , "giallo@mail.com" , "arancione@mail.com"];
+console.log(listaEmail);
+
+// controlla che sia nella lista di chi può accedere
+var message = "EMAIL " + userEmail + " NOT FOUND";
+
+for (i = 0 ; i < listaEmail.length ; i++) {
+
+    // console.log("Iterazione #", i , listaEmail[i]);
+    if (userEmail == listaEmail[i]) {
+        // console.log("EMAIL " + userEmail + " FOUND");
+        message = "EMAIL " + userEmail + " FOUND";
+    } else {
+        // console.log("EMAIL " + userEmail + " NOT FOUND");
+    }
+}
+
+// stampa un messaggio appropriato sull'esito del controllo
+console.log(message);
+document.getElementById("email").innerHTML = message;
