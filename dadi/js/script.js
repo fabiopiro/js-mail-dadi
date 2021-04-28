c6d39a76a81c9ff61ad081945251e285711370b9
fabiopiro <piroddifabio01@gmail.com>
2021-04-28 16:22:34 +0200

    dadi funzionanti con pareggio

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,8 +4,24 @@
 
 
 
-// Generare un numero random da 1 a 6, sia per il giocatore
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+var randomOne = Math.floor ((Math.random()*6) + 1);
+console.log(randomOne);
+document.getElementById("first").innerHTML = "YOU - " + randomOne;
 
-// sia per il computer
+var randomTwo = Math.floor ((Math.random()*6) + 1);
+console.log(randomTwo);
+document.getElementById("second").innerHTML = "COMPUTER - " + randomTwo;
+
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto
+var message = "THE COMPUTER WON with " + randomTwo + " over yours " + randomOne;
+
+if (randomOne > randomTwo) {
+    message = "YOU WIN with " + randomOne + " over the computer's " + randomTwo;
+} else if (randomOne == randomTwo) {
+    message = "DRAW everybody rolled " + randomOne;
+}
+
+console.log(message);
+document.getElementById("result").innerHTML = message;
